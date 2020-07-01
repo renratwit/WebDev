ab3207965785da852646638ed1210bc39352ae47
renr <renr@wit.edu>
2020-07-01 13:55:55 -0400

    leedle leedle leedle lee

--- villagers.js
+++ villagers.js
@@ -1,9 +1,13 @@
 const apiurl = 'http://acnhapi.com/v1/villagers/1';
 
+
 async function getData() {
     const response = await fetch(apiurl);
     const data = await response.json();
-    console.log(data);
+
+    var myDiv = Document.getElementById('image');
+    myDiv.innerHTML = <img src= {data.image_uri}></img>
+    console.log(data.image_uri);
 }
 
 getData();
