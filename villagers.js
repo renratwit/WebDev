8ddd8923f79dac86588822e193787fb93b16b025
renr <renr@wit.edu>
2020-07-01 13:05:56 -0400

    it's a start

--- villagers.js
+++ villagers.js
@@ -1,2 +1,9 @@
-let data = require('./Resources/villagers.JSON');
-console.log(data.Admiral.gender);
+const apiurl = 'http://acnhapi.com/v1/villagers/1';
+
+async function getData() {
+    const response = await fetch(apiurl);
+    const data = await response.json();
+    console.log(data);
+}
+
+getData();
