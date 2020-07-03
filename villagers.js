0a3b9c31d19c12cc7d43f65145e2b43a5ff3d9e2
renr <renr@wit.edu>
2020-07-02 23:51:11 -0400

    Ugly hover effect

--- villagers.js
+++ villagers.js
@@ -1,26 +1,18 @@
 const apiurl = 'http://acnhapi.com/v1/villagers/';
 
-
-// async function getData() {
-//     const response = await fetch(apiurl);
-//     const data = await response.json();
-//     console.log(data.image_uri);
-
-//     Document.getElementById('image').src = data.image_uri;
-//     // var myDiv = Document.getElementById('image');
-//     // myDiv.innerHTML.src = data.image_uri;
-    
-// }
-
-// getData();
 for(var i = 1; i <= 391; i++) {
 var api = "http://acnhapi.com/v1/villagers/".concat(i);
+
 fetch(api)
     .then(res => res.json())
     .then(data => {
         var img = document.createElement("img");
         img.src = data.image_uri;
         document.getElementById('main').appendChild(img);
+        let hoverData = data.name["name-USen"].concat("\n").concat(data.personality);
+        console.log(hoverData);
+        img.title = hoverData;
+        
     });
 
 }
