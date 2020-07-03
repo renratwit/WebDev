f9a8e764cbcfcd4b64cffa097f126d934b34b4c3
renr <renr@wit.edu>
2020-07-02 23:27:05 -0400

    Villager page is lookin spiffy

--- villagers.js
+++ villagers.js
@@ -1,13 +1,26 @@
-const apiurl = 'http://acnhapi.com/v1/villagers/1';
+const apiurl = 'http://acnhapi.com/v1/villagers/';
 
 
-async function getData() {
-    const response = await fetch(apiurl);
-    const data = await response.json();
+// async function getData() {
+//     const response = await fetch(apiurl);
+//     const data = await response.json();
+//     console.log(data.image_uri);
 
-    var myDiv = Document.getElementById('image');
-    myDiv.innerHTML = <img src= {data.image_uri}></img>
-    console.log(data.image_uri);
+//     Document.getElementById('image').src = data.image_uri;
+//     // var myDiv = Document.getElementById('image');
+//     // myDiv.innerHTML.src = data.image_uri;
+    
+// }
+
+// getData();
+for(var i = 1; i <= 391; i++) {
+var api = "http://acnhapi.com/v1/villagers/".concat(i);
+fetch(api)
+    .then(res => res.json())
+    .then(data => {
+        var img = document.createElement("img");
+        img.src = data.image_uri;
+        document.getElementById('main').appendChild(img);
+    });
+
 }
-
-getData();
