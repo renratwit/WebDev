966f2d8d9de7ac14ff33f27be7b8de8c7502ff64
renr <renr@wit.edu>
2020-07-06 20:01:14 -0400

    Added their name under to the images

--- villagers.js
+++ villagers.js
@@ -1,26 +1,52 @@
 const apiurl = 'http://acnhapi.com/v1/villagers/';
-var apiData;
+
 for(var i = 1; i <= 391; i++) {
 var api = "http://acnhapi.com/v1/villagers/".concat(i);
 
 fetch(api)
     .then(res => res.json())
     .then(data => {
+        var main = document.getElementById('main');
+        var villager = document.createElement('villager');
+
         var img = document.createElement("img");
+        var name = document.createElement('name');
+        name.innerHTML = data.name["name-USen"];
+        
+
         img.src = data.image_uri;
-        apiData = data;
-        document.getElementById('main').appendChild(img);
-        let hoverData = data.name["name-USen"].concat("\n").concat(data.personality);
-        console.log(hoverData);
+        villager.appendChild(img);
+        villager.appendChild(name);
+
+        main.appendChild(villager);
+        
+        let hoverData = data.name["name-USen"];
         img.title = hoverData;
 
         img.onclick = function() {
-            document.getElementById('name').innerHTML = data.name["name-USen"];
-            document.getElementById('personality').innerHTML = data.personality;
-            document.getElementById('birthday').innerHTML = data["birthday-string"];
-            document.getElementById('gender').innerHTML = data.gender;
+            document.getElementById('name').innerHTML ="Name: "  + data.name["name-USen"];
+            document.getElementById('personality').innerHTML = "Personality: " + data.personality;
+            document.getElementById('birthday').innerHTML = "Birthday: " + data["birthday-string"];
+            document.getElementById('gender').innerHTML = "Gender: " + data.gender;
         };
 
     });
 
 }
+
+function searchGender(gender){
+    for(var i = 1; i <= 391; i++) {
+        var api = "http://acnhapi.com/v1/villagers/".concat(i);
+
+        fetch(api)
+            .then(res => res.json())
+            .then(data => {
+                if(data.gender === gender) {
+                    var img = document.createElement("img");
+                    img.src = data.image_uri;
+                    document.getElementById('main').appendChild(img);
+                }
+            })
+    }
+}
+
