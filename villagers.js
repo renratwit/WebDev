20d69bb9864f40edcd943b600d4d627e1a7948b9
renr <renr@wit.edu>
2020-07-18 21:58:49 -0400

    filter by personality

--- villagers.js
+++ villagers.js
@@ -1,6 +1,7 @@
 const apiurl = 'http://acnhapi.com/v1/villagers/';
 
 var villagersList = [];
+var selectedVillagerList = [];
 var personality = ["Uchi", "Jock", "Normal", "Peppy", "Smug", "Snooty", "Lazy", "Cranky"];
 
 for(var i = 1; i <= 391; i++) {
@@ -15,12 +16,10 @@ var api = "http://acnhapi.com/v1/villagers/".concat(i);
         villager.classList.add("villager");
 
         var img = document.createElement("img");
-
         var name = document.createElement('div');
         name.classList.add("name");
         name.innerHTML = data.name["name-USen"];
         
-
         img.src = data.image_uri;
         villager.appendChild(img);
         villager.appendChild(name);
@@ -52,34 +51,45 @@ window.onload = function() {
         var button = document.createElement("button");
         button.innerHTML = personality[i];
         document.getElementById('buttons').appendChild(button);
-        var value = personality[i];
-        button.value = value;
+        button.value = personality[i];
 
         button.addEventListener("click", function() {
             console.log(this.value);
+            selectedVillagerList = [];
             document.getElementById('main').innerHTML = "";
-            for(var i = 0; i < 391; i++) {
+
+            for(var i = 0; i < villagersList.length; i++) {
                 if(villagersList[i].personality === this.value) {
-                    var data = villagersList[i];
+                    console.log("pushing villager")
+                    selectedVillagerList.push(villagersList[i].id);
+                }
+            }
+
+            for(var i = 0; i < selectedVillagerList.length; i++) {
+                var api = "http://acnhapi.com/v1/villagers/".concat(selectedVillagerList[i]);
+
+                fetch(api)
+                .then(res => res.json())
+                .then(data => {
+                    villagersList.push(data);
+                    var main = document.getElementById('main');
                     var villager = document.createElement('div');
                     villager.classList.add("villager");
-
+            
                     var img = document.createElement("img");
-
                     var name = document.createElement('div');
                     name.classList.add("name");
                     name.innerHTML = data.name["name-USen"];
                     
-
                     img.src = data.image_uri;
                     villager.appendChild(img);
                     villager.appendChild(name);
-
+            
                     main.appendChild(villager);
                     
                     let hoverData = data.name["name-USen"];
                     img.title = hoverData;
-
+            
                     img.onclick = function() {
                         console.log("image is clicked");
                         document.getElementById('name').innerHTML ="Name: "  + data.name["name-USen"];
@@ -88,12 +98,13 @@ window.onload = function() {
                         document.getElementById('birthday').innerHTML = "Birthday: " + data["birthday-string"];
                         document.getElementById('gender').innerHTML = "Gender: " + data.gender;
                     };
-
-                }
+            
+                });//end api fetch;
             }
+            
+        }); //end button eventListener
 
-        });
-    }
+    }//end personality for-loop
 
     //search by name
     var searchBtn = document.getElementById('search_button');
@@ -106,5 +117,26 @@ window.onload = function() {
     
 }
 
+function insertVillagers(arr) {
+    console.log("inserting villagers");
+    for(var i = 0; i < arr.length; i++) {
+        var data = arr[i];
+        var main = document.getElementById('main');
+        var main = document.getElementById('main');
+        var villager = document.createElement('div');
+        villager.classList.add("villager");
+        var img = document.createElement("img");
+        var name = document.createElement('div');
+        name.classList.add("name");
 
+        name.innerHTML = data.name["name-USen"];
+        img.src = data.img_uri;
+        villager.appendChild(img);
+        villager.appendChild(name);
+
+        main.appendChild(villager);
+        
+    }
+}
+ 
 
