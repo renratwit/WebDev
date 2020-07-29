84feebfd3ae2f8a75acdbf47d8316019aca82428
renr <renr@wit.edu>
2020-07-29 14:35:26 -0400

    ignore this

--- music.js
+++ music.js
@@ -7,6 +7,7 @@ for(var i = 1; i <= 95; i++) {
             var list = document.getElementById('songs');
             var song = document.createElement("div");
             var image = document.createElement("img");
+            var songName = data.name["name-USen"];
 
             var photo = data.image_uri;
             image.src = photo;
@@ -14,13 +15,14 @@ for(var i = 1; i <= 95; i++) {
             song.append(image);
             song.classList.add("song");
             list.append(song);
-
+            document.getElementById('title').innerHTML = songName;
             var cover = document.getElementById('cover');
             cover.src = photo;
 
             image.addEventListener("click", function(){
                 cover.src = image.src;
                 document.getElementById('music').src = data.music_uri;
+                document.getElementById('title').innerHTML = data.name["name-USen"];
             });
 
             
